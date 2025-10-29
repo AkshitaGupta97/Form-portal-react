846538d8e8924752595586c6e0d7584ca9b88252
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-10-29 16:47:19 +0530

    fully created AddContent Page

--- src/Component/AddContent.jsx
+++ src/Component/AddContent.jsx
@@ -1,13 +1,12 @@
 import { useNavigate } from "react-router-dom";
-import { useContent } from "../Context/ContentContext"
+import { useContent } from "../Context/ContentContext";
 import { useEffect, useState } from "react";
 import { isNotEmpty } from "../Utils/Validations";
-import { parseAst } from "vite";
 
 function AddContent() {
-  const {addContent} = useContent();
+  const { addContent } = useContent();
   const navigate = useNavigate();
-  
+
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [category, setCategory] = useState('Misc');
@@ -16,48 +15,102 @@ function AddContent() {
 
   useEffect(() => {
     const draft = localStorage.getItem('contentDraft');
-    if(draft){
-      try{
-        const {title, body, category} = JSON.parse(draft);
+    if (draft) {
+      try {
+        const { title, body, category } = JSON.parse(draft);
         setTitle(title || '');
         setBody(body || '');
         setCategory(category || '');
-      }
-      catch(error){
-        console.log("Error in Draft", error)
+      } catch (error) {
+        console.log("Error in Draft", error);
       }
     }
-  }, [])
+  }, []);
 
   const handleSubmit = (e) => {
-    e.preventDeafult();
-    if(!isNotEmpty(username) || !isNotEmpty(password)) {
+    e.preventDefault();
+    if (!isNotEmpty(title) || !isNotEmpty(body)) {
       setError('Please fill title and body for Article');
       return;
     }
+
     const newContent = {
       id: Date.now(),
-      title, 
-      body, 
-      category, 
+      title,
+      body,
+      category,
       date: new Date().toISOString()
-    }
+    };
 
     addContent(newContent);
     setTitle('');
     setBody('');
     setCategory('Misc');
     setError('');
-    localStorage.remove('contentDraft');
+    localStorage.removeItem('contentDraft');
     navigate('/view');
-
-  }
+  };
 
   return (
-    <div>
-     
+    <div className="panel">
+      <h2>Add Content</h2>
+      <form onSubmit={handleSubmit}>
+
+        <div className="form-group">
+         <label htmlFor="title">Title : </label>
+          <input type="text" id="title" placeholder="Enter title" 
+            value={title} onChange={(e) => setTitle(e.target.value)} 
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="body">Content : </label>
+          <textarea type="text" id="body" placeholder="Enter content" 
+            value={body} onChange={(e) => setBody(e.target.value)} 
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="category">Category : </label>
+          <select id="category" value={category} 
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value='News'>News</option>
+            <option value='Blog'>Blog</option>
+            <option value='Tutoial'>Tutorial</option>
+            <option value='Misc'>Misc</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label>
+            <input type="checkbox"
+             checked={showPreview} onChange={() => setShowPreview((prev => !prev))} 
+            />
+            Show Preview
+          </label>
+        </div>
+
+        {
+          error && <div className="error">{error}</div>
+        }
+        <button type="submit" className="btn">Add Content</button>
+
+      </form>
+      
+      {
+        showPreview && (
+          <div className="preview">
+            <h3>Preview</h3>
+            <h4>{title || 'Title'}</h4>
+            <p>{body || 'Contente Preview'}</p>
+            <small><b>Category : </b>{category} </small>
+          </div>
+        )
+      }
+
     </div>
-  )
+  );
 }
 
-export default AddContent
+export default AddContent;
