d9a804ab74dae820f0a26f5718fff5c78b2c928b
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-10-29 14:54:36 +0530

    created AddContent file

--- src/Component/AddContent.jsx
+++ src/Component/AddContent.jsx
@@ -1,8 +1,61 @@
+import { useNavigate } from "react-router-dom";
+import { useContent } from "../Context/ContentContext"
+import { useEffect, useState } from "react";
+import { isNotEmpty } from "../Utils/Validations";
+import { parseAst } from "vite";
 
 function AddContent() {
+  const {addContent} = useContent();
+  const navigate = useNavigate();
+  
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [category, setCategory] = useState('Misc');
+  const [error, setError] = useState('');
+  const [showPreview, setShowPreview] = useState(false);
+
+  useEffect(() => {
+    const draft = localStorage.getItem('contentDraft');
+    if(draft){
+      try{
+        const {title, body, category} = JSON.parse(draft);
+        setTitle(title || '');
+        setBody(body || '');
+        setCategory(category || '');
+      }
+      catch(error){
+        console.log("Error in Draft", error)
+      }
+    }
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDeafult();
+    if(!isNotEmpty(username) || !isNotEmpty(password)) {
+      setError('Please fill title and body for Article');
+      return;
+    }
+    const newContent = {
+      id: Date.now(),
+      title, 
+      body, 
+      category, 
+      date: new Date().toISOString()
+    }
+
+    addContent(newContent);
+    setTitle('');
+    setBody('');
+    setCategory('Misc');
+    setError('');
+    localStorage.remove('contentDraft');
+    navigate('/view');
+
+  }
+
   return (
     <div>
-        Add Content
+     
     </div>
   )
 }
